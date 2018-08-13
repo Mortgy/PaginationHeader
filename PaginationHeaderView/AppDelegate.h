bcf6e9da86657945fa2ec73ec437a4a5a365b827
mortgy <m.mortgy@mortgy.com>
2018-08-13 18:17:30 +0300

    Initial Commit

---
+++ PaginationHeaderView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PaginationHeaderView
+//
+//  Created by mortgy on 13.08.2018.
+//  Copyright © 2018 Mortgy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PaginationHeaderView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PaginationHeaderView
+//
+//  Created by mortgy on 13.08.2018.
+//  Copyright © 2018 Mortgy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
